34a2f169c88fc001461beb4d5685d54088f36553
Liuming <lfan.canada@gmail.com>
2021-03-24 20:26:25 +0000

    new-tweet.css format updated

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,29 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+//function createTweetElement takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet.
+
+
+const createTweetElement =
+
+
+
+
+
+
+
+
+
+// tweet data object: //This object is taken directly from the initial-tweets.json file in the data-files directory.
+{
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
