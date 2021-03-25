6c7393b6fa39557068e74d54cfcfbc3269c1fc8c
Liuming <lfan.canada@gmail.com>
2021-03-25 03:02:58 +0000

    ajax function added, create/render/load tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,32 +1,91 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+
+const renderTweets = function (tweets) {
+  // loops through tweets
+  for (const tweet of tweets) {
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    $(".container").append(createTweetElement(tweet));
+  }
+};
+
+$(document).ready(function () {
+
+  const url = `/tweets`;
+
+  $("form").on("submit", function (event) {
+
+    event.preventDefault();
+
+    if()
+
+    const tweetText = $(this).serialize();
+    console.log("text", tweetText);
+
+    $.ajax({
+      url: url,
+      method: "POST",
+      data: tweetText,
+    })
+      .done(() => {
+        // console.log(data);
+        loadTweets();
+      })
+      .fail((err) => {
+        console.log(err.message);
+      })
+  });
+
+  const loadTweets = () => {
+    $.ajax({
+      url: url,
+      method: "GET",
+    })
+      .done((data) => {
+        // console.log(data);
+        renderTweets(data);
+      })
+      .fail((err) => {
+        console.log(err.message);
+      })
+  };
+
+  loadTweets();
+
+});
 
 
-//function createTweetElement takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet.
 
+const createTweetElement = function (tweet) {
 
-const createTweetElement =
+  const today = new Date();
+  const createdOn = new Date(tweet.created_at);
+  const msInDay = 24 * 60 * 60 * 1000;
 
+  createdOn.setHours(0, 0, 0, 0);
+  today.setHours(0, 0, 0, 0);
 
+  const diff = (today - createdOn) / msInDay;
+  console.log(diff);
 
+  const $tweet = `<article class="tweet-container">
+    <!---form for buttton or posting to backend, for database-->
+    <header class="tweet-header">
+      <div>
+        <img src="${tweet.user.avatars}" class="avatar">
+        <span>${tweet.user.name}</span>
+      </div>
+      <span>${tweet.user.handle}</span>
+    </header>
+    <div class="tweet-body">
+      <p>${tweet.content.text}</p>
+    </div>
+    <div>
+      <footer class="tweet-footer">
+        <p>${diff} days ago</p>
+      </footer>
+    </div>
+  </article>`;
 
+  return $tweet;
+};
 
-
-
-
-
-// tweet data object: //This object is taken directly from the initial-tweets.json file in the data-files directory.
-{
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
