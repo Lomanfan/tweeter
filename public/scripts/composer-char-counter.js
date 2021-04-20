e8ace928d10391ffb0457fdfea63f679243cc415
Liuming <lfan.canada@gmail.com>
2021-04-20 23:36:21 +0000

    clean up comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
 
     let counter = $(this).siblings(".numberLimit").find(".counter")
     const numLimit = 140 - $(this).val().length;
-    // console.log($(this).val());
 
     if( numLimit < 0) {
       return counter.html(numLimit).css("color", "red");
